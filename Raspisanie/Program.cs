a63777145001903edd5e3e94bcc19eac105134ea
Tassadar2499 <markevich.roma@gmail.com>
2018-07-03 14:58:27 +0600

    Добавил метод, который возвращает словарь предметов и их сложность из текстового файла.

--- Raspisanie/Program.cs
+++ Raspisanie/Program.cs
@@ -13,16 +13,28 @@ namespace Raspisanie
 		/// Главная точка входа для приложения.
 		/// </summary>
 		[STAThread]
-		static void Main()
-		{
+		static void Main(){
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
 			Application.Run(new formOfClasses());
+			var text = returnDictionaryOfSubjects();
+			var i = 9;
 		}
-		public static string[] returnArrayOfClasses()
-		{
+		public static string[] returnArrayOfClasses(){
 			var text = File.ReadAllText("Classes.txt").Split(new char[] { ' ', '\t', '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
 			return text;
+		}
+		public static Dictionary<string, int> returnDictionaryOfSubjects(){
+			var text = File.ReadAllText("Subjects.txt").Split(new char[] { '\t', '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+			var dictionary = new Dictionary<string, int>();
+			for (int i=0; i<text.Length; i++){
+				var str = text[i];
+				var index = str.LastIndexOf('-');
+				var subject = str.Substring(0,index);
+				var difficult = str.Substring(index+1);
+				dictionary.Add(subject, Int32.Parse(difficult));
+			}
+			return dictionary;
 		}
 	}
 	public class Grades{
