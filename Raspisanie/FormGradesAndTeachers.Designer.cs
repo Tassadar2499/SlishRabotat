76a9b893e3e4e828ac89dad9d96894c03c5b8361
simple_man <markevich.roma@gmail.com>
2018-07-26 23:41:15 +0500

    заботал форму

--- Raspisanie/FormGradesAndTeachers.Designer.cs
+++ Raspisanie/FormGradesAndTeachers.Designer.cs
@@ -30,10 +30,22 @@
         {
             this.Классы = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.addGradeTextBox = new System.Windows.Forms.TextBox();
             this.checkedListBoxOfGrades = new System.Windows.Forms.CheckedListBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.textBoxAddTeacher = new System.Windows.Forms.TextBox();
+            this.checkedListBoxOfTeachers = new System.Windows.Forms.CheckedListBox();
+            this.button8 = new System.Windows.Forms.Button();
             this.Классы.SuspendLayout();
             this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
             this.SuspendLayout();
             // 
             // Классы
@@ -44,52 +56,173 @@
             this.Классы.Location = new System.Drawing.Point(0, 0);
             this.Классы.Name = "Классы";
             this.Классы.SelectedIndex = 0;
-            this.Классы.Size = new System.Drawing.Size(916, 544);
+            this.Классы.Size = new System.Drawing.Size(663, 544);
             this.Классы.TabIndex = 0;
             // 
             // tabPage1
             // 
             this.tabPage1.BackColor = System.Drawing.Color.DarkGray;
+            this.tabPage1.Controls.Add(this.button5);
+            this.tabPage1.Controls.Add(this.button4);
+            this.tabPage1.Controls.Add(this.button3);
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.addGradeTextBox);
             this.tabPage1.Controls.Add(this.checkedListBoxOfGrades);
             this.tabPage1.Location = new System.Drawing.Point(4, 54);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(908, 486);
+            this.tabPage1.Size = new System.Drawing.Size(655, 486);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Классы";
             this.tabPage1.Click += new System.EventHandler(this.TabPageGrades);
             // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(533, 373);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 54);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Отменить добавление";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.CancelAddingDefaultClasses_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(418, 373);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 54);
+            this.button4.TabIndex = 1;
+            this.button4.Text = "Добавить классы по умолчанию";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.AddDefaultGrades_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(269, 373);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(94, 67);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Отсортировать по номеру";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.SortingByNumber_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(164, 373);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 54);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Удалить выбранные классы";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.DeleteSelectingGrades_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(47, 422);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 44);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Добавить класс";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.AddNewGrade_Click);
+            // 
+            // addGradeTextBox
+            // 
+            this.addGradeTextBox.Location = new System.Drawing.Point(8, 373);
+            this.addGradeTextBox.Multiline = true;
+            this.addGradeTextBox.Name = "addGradeTextBox";
+            this.addGradeTextBox.Size = new System.Drawing.Size(114, 43);
+            this.addGradeTextBox.TabIndex = 1;
+            this.addGradeTextBox.TextChanged += new System.EventHandler(this.TextAddGrade);
+            // 
+            // checkedListBoxOfGrades
+            // 
+            this.checkedListBoxOfGrades.FormattingEnabled = true;
+            this.checkedListBoxOfGrades.Location = new System.Drawing.Point(8, 8);
+            this.checkedListBoxOfGrades.Name = "checkedListBoxOfGrades";
+            this.checkedListBoxOfGrades.Size = new System.Drawing.Size(485, 349);
+            this.checkedListBoxOfGrades.TabIndex = 0;
+            this.checkedListBoxOfGrades.SelectedIndexChanged += new System.EventHandler(this.CheckedListBoxOfGrades_SelectingItem);
+            // 
             // tabPage2
             // 
             this.tabPage2.BackColor = System.Drawing.Color.DarkGray;
+            this.tabPage2.Controls.Add(this.button8);
+            this.tabPage2.Controls.Add(this.button7);
+            this.tabPage2.Controls.Add(this.button6);
+            this.tabPage2.Controls.Add(this.textBoxAddTeacher);
+            this.tabPage2.Controls.Add(this.checkedListBoxOfTeachers);
             this.tabPage2.Location = new System.Drawing.Point(4, 54);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(908, 486);
+            this.tabPage2.Size = new System.Drawing.Size(655, 486);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Учителя";
             this.tabPage2.Click += new System.EventHandler(this.TabPageTeachers);
             // 
-            // checkedListBox1
+            // button7
             // 
-            this.checkedListBoxOfGrades.FormattingEnabled = true;
-            this.checkedListBoxOfGrades.Location = new System.Drawing.Point(8, 8);
-            this.checkedListBoxOfGrades.Name = "checkedListBox1";
-            this.checkedListBoxOfGrades.Size = new System.Drawing.Size(378, 349);
-            this.checkedListBoxOfGrades.TabIndex = 0;
-            this.checkedListBoxOfGrades.SelectedIndexChanged += new System.EventHandler(this.CheckedListBoxOfGradesSelectingItem);
+            this.button7.Location = new System.Drawing.Point(523, 231);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 54);
+            this.button7.TabIndex = 3;
+            this.button7.Text = "Удалить выбранных учителей";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.DeleteSelectingTeachers_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(388, 181);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 48);
+            this.button6.TabIndex = 2;
+            this.button6.Text = "Добавить учителя";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.AddTeacher_Click);
+            // 
+            // textBoxAddTeacher
+            // 
+            this.textBoxAddTeacher.Location = new System.Drawing.Point(388, 121);
+            this.textBoxAddTeacher.Multiline = true;
+            this.textBoxAddTeacher.Name = "textBoxAddTeacher";
+            this.textBoxAddTeacher.Size = new System.Drawing.Size(100, 54);
+            this.textBoxAddTeacher.TabIndex = 1;
+            this.textBoxAddTeacher.TextChanged += new System.EventHandler(this.TextBoxAddTeacher);
+            // 
+            // checkedListBoxOfTeachers
+            // 
+            this.checkedListBoxOfTeachers.FormattingEnabled = true;
+            this.checkedListBoxOfTeachers.Location = new System.Drawing.Point(8, 8);
+            this.checkedListBoxOfTeachers.Name = "checkedListBoxOfTeachers";
+            this.checkedListBoxOfTeachers.Size = new System.Drawing.Size(344, 319);
+            this.checkedListBoxOfTeachers.TabIndex = 0;
+            this.checkedListBoxOfTeachers.SelectedIndexChanged += new System.EventHandler(this.CheckedListBoxOfTeachers_SelectingItem);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(388, 304);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(100, 70);
+            this.button8.TabIndex = 4;
+            this.button8.Text = "Отсортировать в алфавитном порядке";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.SortingByAlphabet_Click);
             // 
             // FormGradesAndTeachers
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(913, 543);
+            this.ClientSize = new System.Drawing.Size(668, 543);
             this.Controls.Add(this.Классы);
             this.Name = "FormGradesAndTeachers";
             this.Text = "FormGradesAndTeachers";
             this.Load += new System.EventHandler(this.FormGradesAndTeachers_Load);
             this.Классы.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -100,5 +233,16 @@
         private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.TabPage tabPage2;
         private System.Windows.Forms.CheckedListBox checkedListBoxOfGrades;
+        private System.Windows.Forms.TextBox addGradeTextBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.CheckedListBox checkedListBoxOfTeachers;
+        private System.Windows.Forms.TextBox textBoxAddTeacher;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
     }
 }
--- Raspisanie/FormGradesAndTeachers.cs
+++ Raspisanie/FormGradesAndTeachers.cs
@@ -12,14 +12,17 @@ namespace Raspisanie
 {
     public partial class FormGradesAndTeachers : Form
     {
-        public static string[] schoolClasses;
         public FormGradesAndTeachers()
         {
             InitializeComponent();
-            if (schoolClasses != null)
-                checkedListBoxOfGrades.Items.AddRange(schoolClasses);
         }
 
+        public void CheckedList_AddItem(string item, CheckedListBox checkedListBox)
+        {
+            if (item != "" && !checkedListBox.Items.Contains(item))
+                checkedListBox.Items.Add(item);
+        }
+        #region
         private void FormGradesAndTeachers_Load(object sender, EventArgs e)
         {
 
@@ -35,9 +38,94 @@ namespace Raspisanie
 
         }
 
-        private void CheckedListBoxOfGradesSelectingItem(object sender, EventArgs e)
+        private void CheckedListBoxOfGrades_SelectingItem(object sender, EventArgs e)
         {
 
         }
+
+        private void TextAddGrade(object sender, EventArgs e)
+        {
+            
+        }
+        #endregion
+
+        private void AddNewGrade_Click(object sender, EventArgs e)
+        {
+            CheckedList_AddItem(addGradeTextBox.Text, checkedListBoxOfGrades);
+            addGradeTextBox.Clear();
+        }//For Grades
+
+        private void DeleteSelectingGrades_Click(object sender, EventArgs e)
+        {
+            var checkedItems = checkedListBoxOfGrades.CheckedItems;
+
+            for (var i = checkedListBoxOfGrades.CheckedItems.Count - 1; i >= 0; i--)
+                checkedListBoxOfGrades.Items.Remove(checkedListBoxOfGrades.CheckedItems[i]);
+        }//For Grades
+
+        private void SortingByNumber_Click(object sender, EventArgs e)
+        {
+            var grades = Program.ListBoxToStrings(checkedListBoxOfGrades).ToArray();
+
+            Array.Sort(grades, new SortingStringByNumber());
+
+            checkedListBoxOfGrades.Items.Clear();
+
+            foreach (var schoolClass in grades)
+                CheckedList_AddItem(schoolClass,checkedListBoxOfGrades);
+        }//For Grades
+
+        private void AddDefaultGrades_Click(object sender, EventArgs e)
+        {
+            var grades = SchedlueMaker.LoadClasses("Classes.txt").Select(a => a.Name);
+
+            foreach (var schoolClass in grades)
+                CheckedList_AddItem(schoolClass,checkedListBoxOfGrades);
+        }//For Grades
+
+        private void CancelAddingDefaultClasses_Click(object sender, EventArgs e)
+        {
+            var grades = SchedlueMaker.LoadClasses("Classes.txt").Select(a => a.Name);
+
+            foreach (var schoolClass in grades)
+                if (grades.Contains(schoolClass))
+                    checkedListBoxOfGrades.Items.Remove(schoolClass);
+        }//For Grades
+
+        //end grades methods
+        //starts teachers methods
+        #region
+        private void CheckedListBoxOfTeachers_SelectingItem(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void TextBoxAddTeacher(object sender, EventArgs e)
+        {
+            
+        }
+        #endregion
+
+        private void AddTeacher_Click(object sender, EventArgs e)
+        {
+            CheckedList_AddItem(textBoxAddTeacher.Text, checkedListBoxOfTeachers);
+            textBoxAddTeacher.Clear();
+        }//For Teachers
+
+        private void DeleteSelectingTeachers_Click(object sender, EventArgs e)
+        {
+            for (var i = checkedListBoxOfTeachers.CheckedItems.Count - 1; i >= 0; i--)
+                checkedListBoxOfTeachers.Items.Remove(checkedListBoxOfTeachers.CheckedItems[i]);
+        }//For Teachers
+
+        private void SortingByAlphabet_Click(object sender, EventArgs e)
+        {
+            var teachers = Program.ListBoxToStrings(checkedListBoxOfTeachers).ToArray();
+
+            Array.Sort(teachers);
+
+            checkedListBoxOfTeachers.Items.Clear();
+            checkedListBoxOfTeachers.Items.AddRange(teachers);
+        }//For Teachers
     }
 }
