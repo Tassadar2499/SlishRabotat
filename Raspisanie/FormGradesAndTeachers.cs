19fe974193ad4edb09c11ccefbf969ec382e98b9
simple_man <markevich.roma@gmail.com>
2018-07-29 18:03:04 +0500

    test

--- Raspisanie/FormGradesAndTeachers.cs
+++ Raspisanie/FormGradesAndTeachers.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Raspisanie
+namespace Raspisanie//
 {
 	public partial class FormGradesAndTeachers : Form
 	{
