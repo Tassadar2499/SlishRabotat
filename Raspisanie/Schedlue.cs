99a55f69024228e4e7bfc5519c0a920004920f6b
Demond98 <Demon.d.98@mail.ru>
2018-08-18 15:50:21 +0500

    Поправил баг

--- Raspisanie/Schedlue.cs
+++ Raspisanie/Schedlue.cs
@@ -29,13 +29,13 @@ namespace Raspisanie
 			schedlue = new List<Subject[]>();
 
 			for (int day = 0; day < maxDay; day++)
-				schedlue.Add(new Subject[7]);
+				schedlue.Add(new Subject[8]);
 
 			schedlueWeights = new List<double[]>
 			{
 				new double[]{43.2, 54.0, 50.4, 43.2, 36.0, 28.8, 21.6, 10.8 },
-				new double[]{45.6, 57.0, 53.2, 45.6, 38.0, 30,4, 22.8, 11.4 },
-				new double[]{57.6, 72.0, 67.2, 57.6, 48.0, 38,4, 28.8, 14.4 },
+				new double[]{45.6, 57.0, 53.2, 45.6, 38.0, 30.4, 22.8, 11.4 },
+				new double[]{57.6, 72.0, 67.2, 57.6, 48.0, 38.4, 28.8, 14.4 },
 				new double[]{48.0, 60.0, 56.0, 48.0, 40.0, 32.0, 24.0, 12.0 },
 				new double[]{42.0, 52.5, 49.0, 42.0, 35.0, 28.0, 21.0, 10.5 },
 				new double[]{38.4, 48.0, 44.8, 38.4, 32.0, 25.6, 19.2, 9.6 }
